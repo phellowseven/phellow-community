b342d59875aa74a7fb5adfacf05673e7bd571653
Maurice Fahn <maurice.fahn@phellowseven.com>
2025-03-27 17:13:36 +0100

    DEV: Create Screenshots using playwright.

---
+++ tests/screenshots/pages.spec.ts
@@ -0,0 +1,25 @@
+import { test } from "@playwright/test";
+
+test.describe("Capture screenshots of pages", () => {
+	test(`Capture login page`, async ({ page: browserPage }, testInfo) => {
+		await browserPage.goto("/login");
+
+		// Wait for any critical elements or animations to complete
+		await browserPage.waitForLoadState("networkidle");
+
+		// Take screenshot
+		await browserPage.screenshot({
+			path: `./screenshots/${testInfo.project.name}-login.png`,
+			fullPage: false,
+		});
+
+		await browserPage.getByRole("button").click();
+
+		await browserPage.waitForTimeout(2000);
+
+		await browserPage.screenshot({
+			path: `./screenshots/${testInfo.project.name}/dashboard.png`,
+			fullPage: false,
+		});
+	});
+});
