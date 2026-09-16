69bc07cbef46fda51aa8a640f80c0dca6b09e920
Maurice Fahn <maurice.fahn@phellowseven.com>
2026-09-16 12:36:10 +0200

    DEV: Updated dependencies (stage 4 – Storybook 10; drop unwired addon-vitest).

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,6 @@ const config: StorybookConfig = {
 		"@chromatic-com/storybook",
 		"@storybook/addon-docs",
 		"@storybook/addon-a11y",
-		"@storybook/addon-vitest",
 	],
 	framework: {
 		name: "@storybook/sveltekit",
